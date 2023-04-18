5328cdcb9d3f137eef61c424ac6a2e0d05508475
Brown-Lee <1994756616@qq.com>
2023-04-18 21:41:06 +0800

    底部导航栏

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 import App from './App'
 import store from './store'
 
-// #ifndef VUE3
-import Vue from 'vue'
-Vue.config.productionTip = false
-Vue.prototype.$store = store
-Vue.prototype.$adpid = "1111111111"
-Vue.prototype.$backgroundAudioData = {
-	playing: false,
-	playTime: 0,
-	formatedPlayTime: '00:00:00'
-}
-App.mpType = 'app'
-const app = new Vue({
-	store,
-	...App
-})
-app.$mount()
-// #endif
+// // #ifndef VUE3
+// import Vue from 'vue'
+// Vue.config.productionTip = false
+// Vue.prototype.$store = store
+// Vue.prototype.$adpid = "1111111111"
+// Vue.prototype.$backgroundAudioData = {
+// 	playing: false,
+// 	playTime: 0,
+// 	formatedPlayTime: '00:00:00'
+// }
+// App.mpType = 'app'
+// const app = new Vue({
+// 	store,
+// 	...App
+// })
+// app.$mount()
+// // #endif
 
 // #ifdef VUE3
 import {
